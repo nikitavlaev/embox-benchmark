873088e5c4adc519d709efe303f227fb1a112eff
Nikita <nikitavlaev00@gmail.com>
2020-04-25 03:26:30 +0300

    add scripts and desc

--- scripts/rumprun/test.sh
+++ scripts/rumprun/test.sh
@@ -1,5 +1,6 @@
-#сcd ..; export PATH="${PATH}:$(pwd)/rumprun/bin"
-x86_64-rumprun-netbsd-gcc -o helloer-rumprun helloer.c
-rumprun-bake hw_virtio helloer-rumprun.bin helloer-rumprun
-./helloer
-rumprun qemu -i helloer-rumprun.bin 
+export PATH="${PATH}:$(pwd)/rumprun/bin"
+x86_64-rumprun-netbsd-gcc -o time-test-rumprun time_test.c
+rumprun-bake hw_virtio time-test-rumprun.bin  time-test-rumprun
+gcc time_test.c -o time_test
+./time_test | ts 
+rumprun qemu -i time-test-rumprun.bin | ts 
